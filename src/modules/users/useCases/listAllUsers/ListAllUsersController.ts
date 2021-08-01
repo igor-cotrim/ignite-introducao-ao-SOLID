be5e0d0f054b76e0e42abc6480a96abdf33369bd
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-08-01 02:28:58 -0300

    Challenge completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,20 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { ListAllUsersUseCase } from './ListAllUsersUseCase';
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = <{ user_id: string }>request.headers;
+
+    try {
+      const all = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.status(200).send(all);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
